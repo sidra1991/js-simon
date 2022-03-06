bf5bb3bc817dca3b8535aedaaecc86f7bfd40259
sidra1991 <1991sidra@gmail.com>
2022-03-06 18:04:01 +0100

    è da fare meglio ma non cè tempo neanche oggi

--- js/script.js
+++ js/script.js
@@ -17,27 +17,33 @@ for (let generatore = 1; generatore < 6; generatore++) {
 
 
 // Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 secondi allo scadere dei quali li nascondete)
+let superTime = 30000
+let time = 5000;
+setTimeout(numberDeleted,superTime);
 
-setTimeout(numberDeleted,30000);
+
 const remember = []
 
-
 function numberDeleted() {
-    number.innerHTML= ``;
-   
-    // for (let test = 0; test < randomSeries.length; test++) {
-    //     let numberone = prompt(`prova a indovinare il numero ${test+1}`)
-    //     if (numberone == randomSeries[test] ) {
-    //         remember.push( "risposta giusta" );
-    //     }else {
-    //         remember.push("risposta errata");
-    //     }
-
-    //     number.innerHTML += `<div class="cell">  ${remember[test]} </div>`;
-    // }
-
+    number.innerHTML = "";
+    console.log(superTime--)
+    setTimeout(numberDivide,time);
+    for (let volte = 0; volte < randomSeries.length; volte++) {
+        let indovina = prompt (`iserisci il numero del cerchio ${volte+1}`)     
+        if ( indovina == randomSeries[volte] ) {
+            number.innerHTML += `<div class="cell">  indovinato </div>`;
+            function numberDivide() {
+                document.getElementById(`counter`).innerHTML= time--
+            }
+        }else {
+            number.innerHTML += `<div class="cell">  sbagliato </div>`;
+            function numberDivide() {
+                document.getElementById(`counter`).innerHTML= time--
+            }
+        }
+    
+    }
 }
-
 // Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // Consigli del giorno:
