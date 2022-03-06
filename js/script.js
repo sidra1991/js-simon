228359dee59048870b1dfeaf8c74fcdc729f9b2a
sidra1991 <1991sidra@gmail.com>
2022-03-06 16:30:40 +0100

    manca poco

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 // Visualizzare in pagina 5 numeri casuali.
 
-
 const number = document.getElementById(`container`);
 const text = document.getElementById(`output`);
 const randomSeries = []
@@ -18,6 +17,27 @@ for (let generatore = 1; generatore < 6; generatore++) {
 
 
 // Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 secondi allo scadere dei quali li nascondete)
+
+setTimeout(numberDeleted,30000);
+const remember = []
+
+
+function numberDeleted() {
+    number.innerHTML= ``;
+   
+    // for (let test = 0; test < randomSeries.length; test++) {
+    //     let numberone = prompt(`prova a indovinare il numero ${test+1}`)
+    //     if (numberone == randomSeries[test] ) {
+    //         remember.push( "risposta giusta" );
+    //     }else {
+    //         remember.push("risposta errata");
+    //     }
+
+    //     number.innerHTML += `<div class="cell">  ${remember[test]} </div>`;
+    // }
+
+}
+
 // Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // Consigli del giorno:
